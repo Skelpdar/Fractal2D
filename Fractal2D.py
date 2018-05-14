d99f4a1c8d42036685bf9ce51990438f64e3e989
Hrannar <viktor.hrannar.jonsson@gmail.com>
2018-05-14 12:11:39 +0200

    Newton

--- Fractal2D.py
+++ Fractal2D.py
@@ -4,26 +4,35 @@ class Fractal2D:
 	
 	zeroes = np.array([])
 
-	def __init__(function, derivative = None, maxIterations = 100):
+	def __init__(self, function, derivative = None, maxIterations = 100, tolerance = 10E-4):
 		self.function = function
-		self.derivative = None
+		self.derivative = derivative
 		self.maxIterations = maxIterations
-
-	def newton(p):
+		self.tolerance = tolerance
+	
+	def newton(self, p):
 		#Should return the coordinate of the root and the number of iterations it took
 		#return None on failure to find root?	
-		pass
+		for n in range (self.maxIterations):
+			p = np.linalg.solve(self.derivative(p), -self.function(p)) + p
+			if np.linalg.norm(self.function(p)) < self.tolerance:
+				return (p,n)
+		return None
+	
 
-	def findZeroes(root, tolerance):
+	def findZeroes(self, root, tolerance):
 		#Should return the index in 'zeroes' of the root, or an already present one within the given tolerance
 		pass
 
 	def plot():
 		pass
 	
-	def simplifiedNewton(p):
+	def simplifiedNewton(self, p):
 		#Newtons method but with numericalDerivativ
 		pass
 
-	def numericalDerivative(p, stepDistance):
+	def numericalDerivative(self, p, stepDistance):
 		pass
+	
+
+
