ab4f7d6bfef7087cebd3a5d8898e1a4a54c871b2
Jonathan <jonathanpetersson@Jonathans-MacBook-Pro.local>
2018-05-24 13:37:09 +0200

    Plot2
    Added the plot-function I wrote some time ago with the command: meshgrid and pcolor. Doesn't hurt to show that it was possible to solve the task with their recomendations of commands.

--- Fractal2D.py
+++ Fractal2D.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sympy import symbols, diff, Array
 
 class Fractal2D:
@@ -12,9 +13,9 @@ class Fractal2D:
 		self.maxIterations = maxIterations
 		self.tolerance = tolerance
 
-#How a functioned could be written in order for automated derivation to work.
-#def f(x,y):
-#   return Array([[x**3-3*x*y**2-1],[3*x**2*y-y**2]])	
+	#How a functioned could be written in order for automated derivation to work.
+	#def f(x,y):
+	#return Array([[x**3-3*x*y**2-1],[3*x**2*y-y**2]])	
 	def automatedDeriv(self):
 		x, y = symbols('x y')        
 		a = f1_derivative_x = diff(self.function[0],x)  
@@ -24,7 +25,7 @@ class Fractal2D:
 		return np.array([[a,b],[c,d]])
     
 	def newton(self, p):
-		#Should return the coordinate of the root and the number of iterations it took
+		#Should return the coordinate of the root and the number of iterations it took.
 		#return None on failure to find root?	
 		for n in range (self.maxIterations):
 			try:
@@ -35,15 +36,15 @@ class Fractal2D:
 				return None
 		return None
 	
-
 	def findRootIndex(self, root):
-		#Should return the index in 'zeroes' of the root, or an already present one within the given tolerance
+		#Should return the index in 'zeroes' of the root, 
+		#or an already present one within the given tolerance.
 		
-		#Check if the difference to some existing root is smaller than the tolerance
+		#Check if the difference to some existing root is smaller than the tolerance.
 		for index in range(len(self.zeroes)):
 			if np.linalg.norm((self.zeroes[index] - root)) < self.tolerance:
 				return index
-		#If not, add a new root
+		#If not, add a new root.
 		else:
 			self.zeroes.append(root)
 			return len(self.zeroes)-1	
@@ -69,6 +70,24 @@ class Fractal2D:
 		
 		plt.imshow(im, origin = 'lower', extent = [-dim,dim,-dim,dim])
 		
+	def plot2(self, N, a, b, c, d):
+		xvalues = np.linspace(a, b, N)
+		yvalues = np.linspace(c, d, N)
+		G = np.meshgrid(xvalues, yvalues)
+		#A = []
+		for n in range(N):
+			#row = []
+			for i in range(N):
+				root = self.newton(np.array([G[0][n][i], G[1][n][i]]))
+				if root == None:
+					#row.append(5)
+					G[0][n][i] = 5
+				else:
+					#row.append(self.findRootIndex(root[0]))
+					G[0][n][i] = self.findRootIndex(root[0])
+		#vstack(A)
+		#plt.pcolor(A)
+		plt.pcolor(G[0])
 	
 	def simplifiedNewton(self, p):
 		#Newtons method but with numericalDerivativ
@@ -91,6 +110,3 @@ class Fractal2D:
 		f2_derivative_x = (f2x_ad_h-f2x_sub_h)/(2*h) 
 		f2_derivative_y = (f2y_ad_h-f2y_sub_h)/(2*h)
 		return np.array([[f1_derivative_x,f1_derivative_y],[f2_derivative_x,f2_derivative_y]])
-	
-
-
