b3758fd7a14bb8bd833ff3cac5438b39159c6edc
Erik C <erikwilliamclarkson@gmail.com>
2018-05-25 10:34:56 +0200

    Update

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import Fractal2D
 import numpy as np
+from sympy import *
 
+"""
 #Defines the function
 def F(p):
 	x = p[0]
 	y = p[1]
 	return np.array([x**3-3*x*y**2-1,3*x**2*y-y**3])
+"""
+def F(x,y):
+    x,y = symbols('x,y')
+    return Array([[x**3-3*x*y**2-1],[3*x**2*y-y**2]])
 
 #Defines the Jacobian 'derivative'
 def J(p):
@@ -13,7 +19,7 @@ def J(p):
     y = p[1]
     return np.array([[3*x**2-3*y**2,-6*x*y],[6*x*y,3*x**2-2*y]])
 
-fractal = Fractal2D.Fractal2D(F, J, maxIterations = 10, tolerance = 10e-4)
+fractal = Fractal2D.Fractal2D(F, None, maxIterations = 10, tolerance = 10e-4)
 
 #Finds a root
 print(fractal.newton(np.array([1,1])))
@@ -27,3 +33,9 @@ print(fractal.zeroes)
 #This one wont add a new element, because it is close to the previous root
 fractal.findRootIndex(np.array([1,3.000001]))
 print(fractal.zeroes)
+
+"""
+p = np.array([[1],[2]])
+print(fractal.numericalDerivative(p))
+"""
+print(fractal.automatedDeriv())
