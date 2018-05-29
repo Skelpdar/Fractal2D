1b03ceb52f77c52e4a6fc9e8c8da98e6b0eaf911
Skelpdar <skelpdar@gmail.com>
2018-05-29 15:05:14 +0200

    Added additional example

--- example2.py
+++ example2.py
@@ -8,7 +8,15 @@ np.seterr(all = 'raise')
 def F(p):
 	x = p[0]
 	y = p[1]
-	return np.array([x**3-3*x*y**2-1,3*x**2*y-y**3])
+	return np.array([x**8 - 28*x**6*y**2 + 70*x**4*y**4 + 15*x**4 - 28*x**2*y**6 - 90*x**2*y**2 + y**8 + 15*y**4 - 16,
+				  8*x**7*y - 56*x**5*y**3 +56*x**3*y**5 + 60*x**3*y - 8*x*y**7 - 60*x*y**3])
+
+"""
+def F(x,y):
+    x,y = symbols('x,y')
+    return Array([[x**3-3*x*y**2-1],[3*x**2*y-y**2]])
+"""
+
 
 #Defines the Jacobian 'derivative'
 def J(p):
@@ -16,18 +24,21 @@ def J(p):
     y = p[1]
     return np.array([[3*x**2-3*y**2,-6*x*y],[6*x*y,3*x**2-3*y**2]])
 
-#Generates a ton of colours to be used
 colours = []
 for c in range(1,10000):
 	colours.append([np.random.rand(),np.random.rand(),np.random.rand()])
 
-fractal = Fractal2D.Fractal2D(F, J, maxIterations = 500, tolerance = 10e-4, simplified = False)
+fractal = Fractal2D.Fractal2D(F, None, maxIterations = 500, tolerance = 10e-4, simplified = False)
 
 fractal.colours = colours
 
-#Set three nice red, blue and green colours for the first three roots
 fractal.colours[0] = [217/255,34/255,34/255]
 fractal.colours[1] = [36/255,69/255,145/255]
 fractal.colours[2] = [28/255,174/255,28/255]
 
-fractal.plot(-2,2,-1.125,1.125,1280,720, Filename="example1")
+p = np.array([1,1])
+#print(fractal.derivative(p))
+
+print(fractal.numericalDerivative(p))
+
+fractal.plot(-2,2,-1.125,1.125,1280,720)
